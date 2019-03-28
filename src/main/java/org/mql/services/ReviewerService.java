750d8778d6fc12d20166478d5f9ac6e899b63143
AitBassouAli <aitbassouali@gmail.com>
2019-03-28 23:02:16 +0100

    costomize administration page

--- src/main/java/org/mql/services/ReviewerService.java
+++ src/main/java/org/mql/services/ReviewerService.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,9 @@ public class ReviewerService {
 	
 	@Autowired
 	private IReviewerMetier reviewerMetier;
+	@RequestMapping("reviwers")
+	public List<Reviewer> getAll(){
+		return reviewerMetier.getAll();
+	}
 	
 }
